d08ec8cd98c7197d8d9f97263f22a989ae57a18c
isabelagusmao <isabelagusmao93@gmail.com>
2022-11-29 11:29:16 -0300

    correção na pagina de editar estoque

--- src/pages/Estoque/EditarEstoque.vue
+++ src/pages/Estoque/EditarEstoque.vue
@@ -1,15 +1,15 @@
 <template>
-    <q-page>
-        <Formulario-estoque acao="editar"/>
-    </q-page>
-  </template>
-  
-  <script>
-  import FormularioEstoque from "src/components/Estoque/FormularioEstoque.vue";
-  
-  export default {
-    components: {
-      FormularioEstoque,
-    },
-  };
-  </script>
+  <q-page>
+      <formulario-estoque acao="editar"/>
+  </q-page>
+</template>
+
+<script>
+import FormularioEstoque from "src/components/Estoque/FormularioEstoque.vue";
+
+export default {
+  components: {
+    FormularioEstoque,
+  },
+};
+</script>
